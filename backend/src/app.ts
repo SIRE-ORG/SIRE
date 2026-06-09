9e84284c667d95a58e95094545ccec4555fd32b7
Cristian <c.sandoval38@ufromail.cl>
2026-06-09 11:35:47 -0400

    chore: preparar puerto con rutas faltantes  y host de fastify para render

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,20 +1,27 @@
 import Fastify from 'fastify';
 import { userRoutes } from './routes/user.routes.js';
 import { publicationRoutes } from './routes/publication.routes.js';
+import { authRoutes } from './routes/auth.routes.js';
+//import { reservationRoutes } from './routes/reservation.routes.js'; //Aun no implementado
 
 const app = Fastify({ logger: true });
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 app.register(userRoutes, { prefix: '/api/v1/users' });
 app.register(publicationRoutes, { prefix: '/api/v1/publications' });
+app.register(authRoutes, { prefix: '/api/v1/auth' });
+//app.register(reservationRoutes, { prefix: '/api/v1/reservations' }); //Aun no implementado
 
 const start = async () => {
     try {
-        await app.listen({ port: 3000, host: '0.0.0.0' });
-        console.log('SIRE Backend corriendo en http://localhost:3000');
+        const address = await app.listen({
+            port: port,
+            host: '0.0.0.0'
+        });
+        console.log(`Servidor escuchando en ${address}`);
     } catch (err) {
         app.log.error(err);
         process.exit(1);
     }
 };
-
 start();
