af8139bb1af26fac190994a14bce9e0b7fda2608
Cristian <c.sandoval38@ufromail.cl>
2026-06-15 16:38:40 -0400

    fix: Implementación de cancelReservation en routes

--- backend/src/routes/reservation.routes.ts
+++ backend/src/routes/reservation.routes.ts
@@ -2,11 +2,13 @@ import type { FastifyInstance } from 'fastify';
 import {
     createReservation,
     getMyReservations,
-    updateReservationStatus
+    updateReservationStatus,
+    cancelReservation
 } from '../controllers/reservation.controller.js';
 
 export async function reservationRoutes(app: FastifyInstance) {
     app.post('/', createReservation);
     app.get('/mine', getMyReservations);
     app.patch('/:id/status', updateReservationStatus);
+    app.delete('/:id', cancelReservation);
 }
